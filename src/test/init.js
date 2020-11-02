6a36d870240c1240c70e987beb7c3a2f51ecec71
cckeating <ckeat62@gmail.com>
2020-11-02 17:40:11 -0500

    Added integration tests, CI for github actions

---
+++ src/test/init.js
@@ -0,0 +1,21 @@
+/* eslint-disable global-require */
+/* eslint-disable no-undef */
+process.env.NODE_ENV = 'test';
+require('../config/config');
+
+const sequelize = require('../db');
+
+it('Should equal 2', () => {
+  return 1 + 1;
+});
+
+describe('REST API Integration Tests', () => {
+  before(async () => {
+    await sequelize.sync({ force: true });
+  });
+  require('../modules/auth/routes.spec');
+
+  after(async () => {
+    await sequelize.close();
+  });
+});
